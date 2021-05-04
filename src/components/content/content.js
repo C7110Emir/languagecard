0c07a3de6479059bfba5b1d0bb8d1c5df7561dcd
EmirBirlik <ninjakoal2002@gmail.com>
2021-05-04 19:14:59 -0400

    edits

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -2,56 +2,39 @@ import React from 'react'
 import style from "./content.module.css"
 import data from "../data"
 import {useState} from "react"
-function Content() {
-    const [jsdisplay,setJsdisplay] = useState(false)
-    const [cppdisplay,setCppdisplay] = useState(false)
-    const [pydisplay,sePydisplay] = useState(false)
-    const [phpdisplay,setPhpdisplay] = useState(false)
-    const [javadisplay,setJavadisplay] = useState(false)
-    const [htmldisplay,setHtmldisplay] = useState(false)
-    const [godisplay,setGodisplay] = useState(false)
-    const [cssdisplay,setCssdisplay] = useState(false)
-    function handleClick(e) {
-        console.log(e)
-        if(e.target.id == "html"){
-            setHtmldisplay = !htmldisplay
-        }
-        else if(e.target.id == "javascript"){
-            setJsdisplay = !jsdisplay
-        }
-    }
+function Content(props) {
+    const [display,setdisplay] = useState(false)
+    const handleClick = () => {
+        setDisplay(!display);
+      };
     
     return (
-        <div className={style.outerdiv}>
-          
-            <div className={style.innerdiv}>
-                <div className={style.content}>
-                    <h1>Languages</h1>
-                    <div className={style.imgdiv} >
-                    {data.map((element)=>{
-                        return(
-                        <div id={element.name} className={style.innermapdiv} onClick={handleClick}>
-                            <img src={element.img} alt="sdgasdg" className={style.imagee} /> 
-                            <div id={element.name} className={style.factlistdefault} >
-                                <ul>
-                                    <li>{element.options[0]}</li>
-                                    <li>{element.options[1]}</li>
-                                    <li>{element.options[2]}</li>
-                                </ul>
-                                
-                                
-                            </div>                    
-                        </div>
-                        )
-                     } )
-                    }
-                    </div>
-                </div>
-                <div className={style.clip}>
-                
-                </div>   
-            </div>  
-        </div>
-    )
+        <>
+          <div
+            className={display ? 'display' : null}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHoverRes}
+            onClick={handleClick}
+          >
+            {!display ? (
+              <>
+                <img className="logo" src={props.logo} alt="" />
+                <div className="desc">{props.name}</div>
+              </>
+            ) : (
+              <>
+                <ul className="back">
+                  <li>{props.options[0]}</li>
+                  <li>{props.options[1]}</li>
+                  <li>{props.options[2]}</li>
+                </ul>
+              </>
+            )}
+          </div>
+        </>
+      );
+    }
+    
+   
 } 
 export default Content;
--- src/components/fullpage.js
+++ src/components/fullpage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Image from "./image/image"
 import "./fullpage.css"
-import Content from "./content/content"
+import data from "../data"
 function Fullpage() {
+    const arr = lang_data.map((item) => (
+    <Langs name={item.name} logo={item.img} options={item.options} />
+            ));
     return (
-        <div>
-            <Image/>
-            <Content/>
-        </div>
+        
     )
-}
+
 
 export default Fullpage;
