60a8cf5df5b14f4f089cf9ee10ad53cf506ef0d7
EmirBirlik <ninjakoal2002@gmail.com>
2021-05-04 16:01:19 -0400

    edits

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import style from "./content.module.css"
-import cpp from "./cpp.png"
+import data from "../data"
 
 function Content() {
     return (
         <div className={style.outerdiv}>
+          
             <div className={style.innerdiv}>
+                <div className={style.content}>
+                    <h1>Languages</h1>
+                    {data.map((element)=>{
+                        {console.log(element.img)}
+                        <img data-src={element.img} alt="sfasa"/>
+                     } )
+                    }
+                </div>
+                <div className={style.clip}>
                 
+                </div>   
             </div>  
         </div>
     )
